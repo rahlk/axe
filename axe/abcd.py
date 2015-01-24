1417b4d831b6d572f17a75c91dcf13dd26c06910
Rahul Krishna <i.m.ralk@gmail.com>
2015-01-23 22:20:27 -0500

    abcd now has a switch to turn of prints

--- axe/abcd.py
+++ axe/abcd.py
@@ -3,8 +3,9 @@ import sys
 sys.dont_write_bytecode = True
 
 class Abcd:
-  def __init__(i, db = "all", rx = "all"):
+  def __init__(i, db = "all", rx = "all", show = False):
     i.db = db; i.rx = rx;
+    i.show = show
     i.yes = i.no = 0
     i.known = {}; i.a = {}; i.b = {}; i.c = {}; i.d = {}
   def __call__(i, actual = None, predicted = None):
@@ -28,13 +29,14 @@ class Abcd:
     if (i.known[x] == 1):
       i.a[x] = i.yes + i.no
   def header(i):
-    print "#", ('{0:20s} {1:11s}  {2:4s}  {3:4s} {4:4s} ' + \
-           '{5:4s}{6:4s} {7:3s} {8:3s} {9:3s} ' + \
-           '{10:3s} {11:3s}{12:3s}            {13:30s}').format(
-      "db", "rx",
-     "n", "a", "b", "c", "d", "acc", "pd", "pf", "prec",
-      "f", "g", "class")
-    print '-' * 100
+    if i.show:
+      print "#", ('{0:20s} {1:11s}  {2:4s}  {3:4s} {4:4s} ' + \
+             '{5:4s}{6:4s} {7:3s} {8:3s} {9:3s} ' + \
+             '{10:3s} {11:3s}{12:3s}            {13:30s}').format(
+        "db", "rx",
+       "n", "a", "b", "c", "d", "acc", "pd", "pf", "prec",
+        "f", "g", "class")
+      print '-' * 100
   def ask(i, raw = True):
     def p(y) : return int(100 * y + 0.5)
     def n(y) : return int(y)
@@ -48,21 +50,22 @@ class Abcd:
       if (1 - pf + pd): g = 2 * (1 - pf) * pd / (1 - pf + pd)
       if (prec + pd): f = 2 * prec * pd / (prec + pd)
       if (i.yes + i.no): acc = i.yes / (i.yes + i.no)
-      print "#", ('{0:20s} {1:10s} {2:4d} {3:4d} {4:4d} ' + \
+      if i.show:
+        print "#", ('{0:20s} {1:10s} {2:4d} {3:4d} {4:4d} ' + \
           '{5:4d} {6:4d} {7:4d} {8:3d} {9:3d} ' + \
          '{10:3d} {11:3d} {12:3d}           {13:30s}').format(i.db,
           i.rx, n(b + d), n(a), n(b), n(c), n(d),
           p(acc), p(pd), p(pf), p(prec), p(f), p(g), str(x) + ' Bugs')
       return [p(acc), p(pd), p(pf), p(prec), p(f), p(g)]
 
-def _Abcd(before = None, after = None):
-  abcd = Abcd(db = 'Actual', rx = 'Predicted')
+def _Abcd(before = None, after = None, show = False):
+  abcd = Abcd(db = 'Actual', rx = 'Predicted', show = show)
   train = before
   test = after
   # random.shuffle(test)
   for actual, predicted in zip(train, test):
     abcd.tell(actual, predicted)
-  abcd.header()
+  if show: abcd.header()
   return abcd.ask()
 
 """
