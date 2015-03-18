87a6a077536c4c098d6ebf6eae0d994f1537dc4c
Rahul Krishna <i.m.ralk@gmail.com>
2015-03-18 15:00:28 -0400

    latex stuff in rdivDemo

--- axe/sk.py
+++ axe/sk.py
@@ -9,23 +9,34 @@
 
 """
 from __future__ import division
-import sys, random, math
+import sys
+import random
+import math
+from functools import reduce
 sys.dont_write_bytecode = True
 """
 
 #### Standard Utils
 
 """
+
+
 class o():
+
   "Anonymous container"
-  def __init__(i, **fields) :
+  def __init__(i, **fields):
     i.override(fields)
-  def override(i, d): i.__dict__.update(d); return i
+
+  def override(i, d):
+    i.__dict__.update(d)
+    return i
+
   def __repr__(i):
     d = i.__dict__
     name = i.__class__.__name__
     return name + '{' + ' '.join([':%s %s' % (k, pretty(d[k]))
-                     for k in i.show()]) + '}'
+                                  for k in i.show()]) + '}'
+
   def show(i):
     return [k for k in sorted(i.__dict__.keys())
             if not "_" in k]
@@ -41,20 +52,25 @@ exp = lambda n: math.e ** n
 ln = lambda n: math.log(n, math.e)
 g = lambda n: round(n, 2)
 
-def median(lst, ordered = False):
-  if not ordered: lst = sorted(lst)
+
+def median(lst, ordered=False):
+  if not ordered:
+    lst = sorted(lst)
   n = len(lst)
   p = n // 2
-  if n % 2: return lst[p]
+  if n % 2:
+    return lst[p]
   q = p - 1
   q = max(0, min(q, n))
   return (lst[p] + lst[q]) / 2
+
 
 def msecs(f):
   import time
   t1 = time.time()
   f()
   return (time.time() - t1) * 1000
+
 
 def pairs(lst):
   "Return all pairs of items i,i+1 from a list."
@@ -63,10 +79,11 @@ def pairs(lst):
     yield last, i
     last = i
 
-def xtile(lst, lo = 0, hi = 1, width = 50,
-             chops = [0.25, 0.5, 0.75],
-             marks = ["-" , " ", "-"],
-             bar = "|", star = "*", show = " %0.2f"):
+
+def xtile(lst, lo=0, hi=1, width=50,
+          chops=[0.25, 0.5, 0.75],
+          marks=["-", " ", "-"],
+          bar="|", star="*", show=" %0.2f"):
   """The function _xtile_ takes a list of (possibly)
   unsorted numbers and presents them as a horizontal
   xtile chart (in ascii format). The default is a
@@ -74,16 +91,19 @@ def xtile(lst, lo = 0, hi = 1, width = 50,
   10,30,50,70,90 breaks in the data (but this can be
   changed- see the optional flags of the function).
   """
-  def pos(p)   : return ordered[int(len(lst) * p)]
-  def place(x) :
+  def pos(p):
+    return ordered[int(len(lst) * p)]
+
+  def place(x):
     return int(width * float((x - lo)) / (hi - lo + 0.00001))
-  def pretty(lst) :
+
+  def pretty(lst):
     return ', '.join([str(show % x) for x in lst])
   ordered = sorted(lst)
   lo = min(lo, ordered[0])
   hi = max(hi, ordered[-1])
-  what = [pos(p)   for p in chops]
-  where = [place(n) for n in  what]
+  what = [pos(p) for p in chops]
+  where = [place(n) for n in what]
   out = [" "] * width
   for one, two in pairs(where):
     for i in range(one, two):
@@ -93,11 +113,12 @@ def xtile(lst, lo = 0, hi = 1, width = 50,
   out[place(pos(0.5))] = star
   return '(' + ''.join(out) + ")," + pretty(what)
 
-def _tileX() :
+
+def _tileX():
   import random
   random.seed(1)
   nums = [random.random() ** 2 for _ in range(100)]
-  print xtile(nums, lo = 0, hi = 1.0, width = 25, show = " %0.3E")
+  print xtile(nums, lo=0, hi=1.0, width=25, show=" %0.3E")
 """
 
 ### Standard Accumulator for Numbers
@@ -108,16 +129,23 @@ Warning: this accumulator keeps _all_ numbers. Might be better to use
 a bounded cache.
 
 """
+
+
 class Num:
+
   "An Accumulator for numbers"
-  def __init__(i, name, inits = []):
+  def __init__(i, name, inits=[]):
     i.n = i.m2 = i.mu = 0.0
     i.all = []
     i._median = None
     i.name = name
     i.rank = 0
-    for x in inits: i.add(x)
-  def s(i)       : return (i.m2 / (i.n - 1)) ** 0.5
+    for x in inits:
+      i.add(x)
+
+  def s(i):
+    return (i.m2 / (i.n - 1)) ** 0.5
+
   def add(i, x):
     i._median = None
     i.n += 1
@@ -125,21 +153,27 @@ class Num:
     delta = x - i.mu
     i.mu += delta * 1.0 / i.n
     i.m2 += delta * (x - i.mu)
+
   def __add__(i, j):
     return Num(i.name + j.name, i.all + j.all)
+
   def quartiles(i):
-    def p(x) : return int(100 * g(xs[x]))
+    def p(x):
+      return int(100 * g(xs[x]))
     i.median()
     xs = i.all
     n = int(len(xs) * 0.25)
-    return p(n) , p(2 * n) , p(3 * n)
+    return p(n), p(2 * n), p(3 * n)
+
   def median(i):
     if not i._median:
       i.all = sorted(i.all)
       i._median = median(i.all)
     return i._median
+
   def __lt__(i, j):
     return i.median() < j.median()
+
   def spread(i):
     i.all = sorted(i.all)
     n1 = i.n * 0.25
@@ -157,15 +191,20 @@ class Num:
 ### The A12 Effect Size Test
 
 """
+
+
 def a12slow(lst1, lst2):
   "how often is x in lst1 more than y in lst2?"
   more = same = 0.0
   for x in lst1:
     for y in lst2:
-      if    x == y : same += 1
-      elif  x > y : more += 1
+      if x == y:
+        same += 1
+      elif x > y:
+        more += 1
   x = (more + 0.5 * same) / (len(lst1) * len(lst2))
   return x
+
 
 def a12(lst1, lst2):
   "how often is x in lst1 more than y in lst2?"
@@ -175,27 +214,34 @@ def a12(lst1, lst2):
       h2 = t2.l[t2.j]
       h3 = t2.l[t2.j + 1] if t2.j + 1 < t2.n else None
       if h1 > h2:
-        t1.j += 1; t1.gt += t2.n - t2.j
+        t1.j += 1
+        t1.gt += t2.n - t2.j
       elif h1 == h2:
-        if h3 and h1 > h3 :
-            t1.gt += t2.n - t2.j - 1
-        t1.j += 1; t1.eq += 1; t2.eq += 1
+        if h3 and h1 > h3:
+          t1.gt += t2.n - t2.j - 1
+        t1.j += 1
+        t1.eq += 1
+        t2.eq += 1
       else:
         t2, t1 = t1, t2
     return t.gt * 1.0, t.eq * 1.0
   #--------------------------
-  lst1 = sorted(lst1, reverse = True)
-  lst2 = sorted(lst2, reverse = True)
+  lst1 = sorted(lst1, reverse=True)
+  lst2 = sorted(lst2, reverse=True)
   n1 = len(lst1)
   n2 = len(lst2)
-  t1 = o(l = lst1, j = 0, eq = 0, gt = 0, n = n1)
-  t2 = o(l = lst2, j = 0, eq = 0, gt = 0, n = n2)
+  t1 = o(l=lst1, j=0, eq=0, gt=0, n=n1)
+  t2 = o(l=lst2, j=0, eq=0, gt=0, n=n2)
   gt, eq = loop(t1, t1, t2)
   return gt / (n1 * n2) + eq / 2 / (n1 * n2)
 
+
 def _a12():
-  def f1(): return a12slow(l1, l2)
-  def f2(): return a12(l1, l2)
+  def f1():
+    return a12slow(l1, l2)
+
+  def f2():
+    return a12(l1, l2)
   for n in [100, 200, 400, 800, 1600, 3200, 6400]:
     l1 = [rand() for _ in xrange(n)]
     l2 = [rand() for _ in xrange(n)]
@@ -235,10 +281,15 @@ are different, many times sample with replacement
 from both to generate _(y1,z1), (y2,z2), (y3,z3)_.. etc.
 
 """
+
+
 def sampleWithReplacement(lst):
   "returns a list same size as list"
-  def any(n)  : return random.uniform(0, n)
-  def one(lst): return lst[ int(any(len(lst))) ]
+  def any(n):
+    return random.uniform(0, n)
+
+  def one(lst):
+    return lst[int(any(len(lst)))]
   return [one(lst) for _ in lst]
 """
 
@@ -254,18 +305,22 @@ is the difference between the two populations, muted by the
 joint standard deviation of the populations.
 
 """
+
+
 def testStatistic(y, z):
-    """Checks if two means are different, tempered
-     by the sample size of 'y' and 'z'"""
-    tmp1 = tmp2 = 0
-    for y1 in y.all: tmp1 += (y1 - y.mu) ** 2
-    for z1 in z.all: tmp2 += (z1 - z.mu) ** 2
-    s1 = (float(tmp1) / (y.n - 1)) ** 0.5
-    s2 = (float(tmp2) / (z.n - 1)) ** 0.5
-    delta = z.mu - y.mu
-    if s1 + s2:
-      delta = delta / ((s1 / y.n + s2 / z.n) ** 0.5)
-    return delta
+  """Checks if two means are different, tempered
+   by the sample size of 'y' and 'z'"""
+  tmp1 = tmp2 = 0
+  for y1 in y.all:
+    tmp1 += (y1 - y.mu) ** 2
+  for z1 in z.all:
+    tmp2 += (z1 - z.mu) ** 2
+  s1 = (float(tmp1) / (y.n - 1)) ** 0.5
+  s2 = (float(tmp2) / (z.n - 1)) ** 0.5
+  delta = z.mu - y.mu
+  if s1 + s2:
+    delta = delta / ((s1 / y.n + s2 / z.n) ** 0.5)
+  return delta
 """
 
 The rest is just details:
@@ -277,19 +332,29 @@ The rest is just details:
 + For more details see [the Efron text][efron01].
 
 """
-def bootstrap(y0, z0, conf = 0.01, b = 1000):
+
+
+def bootstrap(y0, z0, conf=0.01, b=1000):
   """The bootstrap hypothesis test from
      p220 to 223 of Efron's book 'An
     introduction to the boostrap."""
   class total():
+
     "quick and dirty data collector"
-    def __init__(i, some = []):
-      i.sum = i.n = i.mu = 0 ; i.all = []
-      for one in some: i.put(one)
+    def __init__(i, some=[]):
+      i.sum = i.n = i.mu = 0
+      i.all = []
+      for one in some:
+        i.put(one)
+
     def put(i, x):
-      i.all.append(x);
-      i.sum += x; i.n += 1; i.mu = float(i.sum) / i.n
-    def __add__(i1, i2): return total(i1.all + i2.all)
+      i.all.append(x)
+      i.sum += x
+      i.n += 1
+      i.mu = float(i.sum) / i.n
+
+    def __add__(i1, i2):
+      return total(i1.all + i2.all)
   y, z = total(y0), total(z0)
   x = y + z
   tobs = testStatistic(y, z)
@@ -306,27 +371,30 @@ def bootstrap(y0, z0, conf = 0.01, b = 1000):
 #### Examples
 
 """
+
+
 def _bootstraped():
-  def worker(n = 1000,
-             mu1 = 10, sigma1 = 1,
-             mu2 = 10.2, sigma2 = 1):
-    def g(mu, sigma) : return random.gauss(mu, sigma)
+  def worker(n=1000,
+             mu1=10, sigma1=1,
+             mu2=10.2, sigma2=1):
+    def g(mu, sigma):
+      return random.gauss(mu, sigma)
     x = [g(mu1, sigma1) for i in range(n)]
     y = [g(mu2, sigma2) for i in range(n)]
     return n, mu1, sigma1, mu2, sigma2, \
         'different' if bootstrap(x, y) else 'same'
   # very different means, same std
-  print worker(mu1 = 10, sigma1 = 10,
-               mu2 = 100, sigma2 = 10)
+  print worker(mu1=10, sigma1=10,
+               mu2=100, sigma2=10)
   # similar means and std
-  print worker(mu1 = 10.1, sigma1 = 1,
-               mu2 = 10.2, sigma2 = 1)
+  print worker(mu1=10.1, sigma1=1,
+               mu2=10.2, sigma2=1)
   # slightly different means, same std
-  print worker(mu1 = 10.1, sigma1 = 1,
-               mu2 = 10.8, sigma2 = 1)
+  print worker(mu1=10.1, sigma1=1,
+               mu2=10.8, sigma2=1)
   # different in mu eater by large std
-  print worker(mu1 = 10.1, sigma1 = 10,
-               mu2 = 10.8, sigma2 = 1)
+  print worker(mu1=10.1, sigma1=10,
+               mu2=10.8, sigma2=1)
 """
 
 Output:
@@ -350,6 +418,8 @@ To reduce that runtime, I avoid things like an all-pairs comparison of all treat
 the effect size test (and only go to bootstrapping in effect size passes:
 
 """
+
+
 def different(l1, l2):
   # return bootstrap(l1,l2) and a12(l2,l1)
   return a12(l2, l1) and bootstrap(l1, l2)
@@ -381,17 +451,20 @@ are called on only a logarithmic number of times. So...
 For examples on using this code, see _rdivDemo_ (below).
 
 """
-def scottknott(data, cohen = 0.3, small = 3, useA12 = False, epsilon = 0.01):
+
+
+def scottknott(data, cohen=0.3, small=3, useA12=False, epsilon=0.01):
   """Recursively split data, maximizing delta of
   the expected value of the mean before and
   after the splits.
   Reject splits with under 3 items"""
-  all = reduce(lambda x, y:x + y, data)
+  all = reduce(lambda x, y: x + y, data)
   same = lambda l, r: abs(l.median() - r.median()) <= all.s() * cohen
   if useA12:
-    same = lambda l, r:   not different(l.all, r.all)
-  big = lambda    n: n > small
+    same = lambda l, r: not different(l.all, r.all)
+  big = lambda n: n > small
   return rdiv(data, all, minMu, big, same, epsilon)
+
 
 def rdiv(data,  # a list of class Nums
          all,  # all the data combined into one num
@@ -403,10 +476,10 @@ def rdiv(data,  # a list of class Nums
   Recurses into each split. Assigns a 'rank' number
   to all the leaf splits found in this way.
   """
-  def recurse(parts, all, rank = 0):
+  def recurse(parts, all, rank=0):
     "Split, then recurse on each part."
     cut, left, right = maybeIgnore(div(parts, all, big, epsilon),
-                                 same, parts)
+                                   same, parts)
     if cut:
       # if cut, rank "right" higher than "left"
       rank = recurse(parts[:cut], left, rank) + 1
@@ -419,12 +492,15 @@ def rdiv(data,  # a list of class Nums
   recurse(sorted(data), all)
   return data
 
-def maybeIgnore((cut, left, right), same, parts):
+
+def maybeIgnore(xxx_todo_changeme, same, parts):
+  (cut, left, right) = xxx_todo_changeme
   if cut:
     if same(sum(parts[:cut], Num('upto')),
             sum(parts[cut:], Num('above'))):
       cut = left = right = None
   return cut, left, right
+
 
 def minMu(parts, all, big, epsilon):
   """Find a cut in the parts that maximizes
@@ -443,7 +519,8 @@ def minMu(parts, all, big, epsilon):
         before, cut, left, right = now, i, l, r
   return cut, left, right
 
-def leftRight(parts, epsilon = 0.01):
+
+def leftRight(parts, epsilon=0.01):
   """Iterator. For all items in 'parts',
   return everything to the left and everything
   from here to the end. For reasons of
@@ -454,7 +531,8 @@ def leftRight(parts, epsilon = 0.01):
   n = j = len(parts) - 1
   while j > 0:
     rights[j] = parts[j]
-    if j < n: rights[j] += rights[j + 1]
+    if j < n:
+      rights[j] += rights[j + 1]
     j -= 1
   left = parts[0]
   for i, one in enumerate(parts):
@@ -469,67 +547,78 @@ def leftRight(parts, epsilon = 0.01):
 Driver for the demos:
 
 """
-def rdivDemo(data, isLatex = False):
+
+
+def rdivDemo(data, isLatex=False):
   if isLatex:
-    print(r"""\documentclass{article}
-    \usepackage{colortbl} % not sure if needed
-    \usepackage[table]{xcolor} % not sure if needed
-    %%%% needed %%%
-    \usepackage{picture}
-    \newcommand{\quart}[4]{\begin{picture}(100,6)%1
-    {\color{black}\put(#3,3){\circle*{4}}\put(#1,3){\line(1,0){#2}}}\end{picture}}
-    \begin{document}
-    """)
+    #     print(r"""\documentclass{article}
+    #     \usepackage{colortbl} % not sure if needed
+    #     \usepackage[table]{xcolor} % not sure if needed
+    #     %%%% needed %%%
+    #     \usepackage{picture}
+    #     \newcommand{\quart}[4]{\begin{picture}(100,6)%1
+    # {\color{black}\put(#3,3){\circle*{4}}\put(#1,3){\line(1,0){#2}}}\end{picture}}
+    #     \begin{document}
+    #     """)
     def z(x):
-        return int(100 * (x - lo) / (hi - lo + 0.00001))
-    data = map(lambda lst:Num(lst[0], lst[1:]),
+      return int(100 * (x - lo) / (hi - lo + 0.00001))
+    data = map(lambda lst: Num(lst[0], lst[1:]),
                data)
     print ""
     ranks = []
-    for x in scottknott(data, useA12 = True):
+    for x in scottknott(data, useA12=True):
       ranks += [(x.rank, x.median(), x)]
     all = []
     for _, __, x in sorted(ranks):
       all += x.quartiles()
     all = sorted(all)
     lo, hi = all[0], all[-1]
-    print "{\\scriptsize \\begin{tabular}{l@{~~~}l@{~~~}r@{~~~}r@{~~~}c}"
-    print "\\arrayrulecolor{darkgray}"
-    print '\\rowcolor[gray]{.9}  rank & treatment & median & IQR & \\\\'  # min= %s, max= %s\\\\' % (int(lo),int(hi))
+    print "{\\normalsize \\begin{tabular}{|l@{~~~}|l@{~~~}|r@{~~~}|r@{~~~}|c|}"
+    print "\hline"
+    # min= %s, max= %s\\\\' % (int(lo),int(hi))
+    print 'Rank & Treatment & Median & IQR & \\\\'
     last = None
     for _, __, x in sorted(ranks):
       q1, q2, q3 = x.quartiles()
       pre = ""
-      if not last == None and not last == x.rank:
+      if not last is None and not last == x.rank:
         pre = "\\hline"
       print pre, '%2s & %12s &    %s  &  %s & \quart{%s}{%s}{%s}{%s} \\\\' % \
-          (x.rank + 1, x.name, float(q2/100), float((q3 - q1)/100), z(q1), z(q3) - z(q1), z(q2), z(100))
+          (x.rank + 1,
+           x.name,
+           float(q2 / 100),
+              float((q3 - q1) / 100),
+              z(q1),
+              z(q3) - z(q1),
+              z(q2),
+              z(100))
       last = x.rank
-    print "\\end{tabular}}"
-    print('''
-    \end{document}
-    ''')
+#     print "\\end{tabular}}"
+#     print('''
+#     \end{document}
+#     ''')
 
   else:
     def z(x):
       return int(100 * (x - lo) / (hi - lo + 0.00001))
-    data = map(lambda lst:Num(lst[0], lst[1:]),
+    data = map(lambda lst: Num(lst[0], lst[1:]),
                data)
     print ""
     ranks = []
-    for x in scottknott(data, useA12 = True):
+    for x in scottknott(data, useA12=True):
       ranks += [(x.rank, x.median(), x)]
     all = []
-    for _, __, x in sorted(ranks): all += x.all
+    for _, __, x in sorted(ranks):
+      all += x.all
     all = sorted(all)
     lo, hi = all[0], all[-1]
     line = "----------------------------------------------------"
     last = None
-    print  ('%4s , %12s ,    %s   , %4s ' % \
-                 ('rank', 'name', 'med', 'iqr')) + "\n" + line
+    print ('%4s , %12s ,    %s   , %4s ' %
+           ('rank', 'name', 'med', 'iqr')) + "\n" + line
     for _, __, x in sorted(ranks):
       q1, q2, q3 = x.quartiles()
-      print  ('%4s , %12s ,    %0.2f  ,  %0.2f ' % \
-                   (x.rank + 1, x.name, x.median(), x.spread())) + \
-                xtile(x.all, lo = lo, hi = hi, width = 30)
+      print ('%4s , %12s ,    %0.2f  ,  %0.2f ' %
+             (x.rank + 1, x.name, x.median(), x.spread())) + \
+          xtile(x.all, lo=lo, hi=hi, width=30)
       last = x.rank
