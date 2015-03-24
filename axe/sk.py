1ba24c8442e8f6766f491ef7ddf205944b52f306
rahlk <i.m.ralk@gmail.com>
2015-03-23 20:22:09 -0400

    Local changes to sk.py  macbook pro

--- axe/sk.py
+++ axe/sk.py
@@ -576,7 +576,7 @@ def rdivDemo(data, isLatex=False):
     print "{\\normalsize \\begin{tabular}{|l@{~~~}|l@{~~~}|r@{~~~}|r@{~~~}|c|}"
     print "\hline"
     # min= %s, max= %s\\\\' % (int(lo),int(hi))
-    print 'Rank & Treatment & Median & IQR & \\\\'
+    print r'Rank & Treatment & Median & IQR & \\\hline'
     last = None
     for _, __, x in sorted(ranks):
       q1, q2, q3 = x.quartiles()
@@ -593,7 +593,7 @@ def rdivDemo(data, isLatex=False):
               z(q2),
               z(100))
       last = x.rank
-#     print "\\end{tabular}}"
+    print r"\hline \end{tabular}}"
 #     print('''
 #     \end{document}
 #     ''')
